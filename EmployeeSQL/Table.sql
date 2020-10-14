8544da03838376568702ff6e0c5c322a18200529
anumala thapa <anuthapamagar89@gmail.com>
2020-10-14 17:42:04 -0400

    updated the table sql

--- EmployeeSQL/Table.sql
+++ EmployeeSQL/Table.sql
@@ -45,9 +45,9 @@ CREATE TABLE salaries(
 );
 
 --check the tables
-SELECT * FROM departments
-SELECT * FROM titles
-SELECT * FROM employees
-SELECT * FROM dept_emp
-SELECT * FROM dept_manager
-SELECT * FROM salaries
+SELECT * FROM departments;
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
